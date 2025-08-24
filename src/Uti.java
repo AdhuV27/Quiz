020f2f0eb1214eedc31fa6058ca64840c1527cf1
Adhu <godadhu1212@gmail.com>
2025-08-24 17:21:01 +0530

    Created the basic quiz class.

---
+++ src/Uti.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Uti {
+    public double score = 0;
+    public void create(String name, String op1, String op2, String op3, String op4, int CO) {
+        System.out.println(name + ": \n");
+        System.out.println("Option 1: " + op1);
+        System.out.println("Option 2: " + op2);
+        System.out.println("Option 3: " + op3);
+        System.out.println("Option 4: " + op4+"\n");
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter choice(number only): ");
+        int choice = in.nextInt();
+
+        if (choice == CO){
+            System.out.println("Correct!");
+            score+=1;
+        }
+        else{
+            System.out.println("Wrong answer, (correct: "+CO+") (u might have also entered choice out of valid range.");
+            score -= 0.5;
+        }
+
+    }
+    public void print_score(){
+        System.out.println(score);
+    }
+}
