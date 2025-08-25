48ea2c6108633e9c2cbbdb7774eadb5e50be69a9
Adhu <godadhu1212@gmail.com>
2025-08-25 17:22:39 +0530

    Made some changes and started creating fill in the blanks

--- src/Uti.java
+++ src/Uti.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class Uti {
     public double score = 0;
-    public void create(String name, String op1, String op2, String op3, String op4, int CO) {
-        System.out.println(name + ": \n");
+    public void create_mcq(String name, String op1, String op2, String op3, String op4, int CO) {
+        System.out.println("Q: "+name + ": \n");
         System.out.println("Option 1: " + op1);
         System.out.println("Option 2: " + op2);
         System.out.println("Option 3: " + op3);
@@ -18,11 +18,25 @@ public class Uti {
             score+=1;
         }
         else{
-            System.out.println("Wrong answer, (correct: "+CO+") (u might have also entered choice out of valid range.");
+            System.out.println("Wrong answer, (correct: "+CO+") (u might have also entered choice out of valid range.)");
             score -= 0.5;
         }
 
     }
+
+    public void create_fib(String q, String ans){
+        System.out.println("Q: "+q);
+        System.out.println("Enter answer: ");
+    }
+
+    public void setScore(double score) {
+        this.score = score;
+    }
+
+    public double getScore() {
+        return score;
+    }
+
     public void print_score(){
         System.out.println(score);
     }
