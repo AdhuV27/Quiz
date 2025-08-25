9a31118d1a18d01aa4236c5d176ba85fa7214d7a
Adhu <godadhu1212@gmail.com>
2025-08-25 18:55:19 +0530

    Added FIB type questions and grade display and some other fail safes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         Uti quiz = new Uti();
+        quiz.setScore(0);
+        //MCQ - 1 mark
+        // FIB - 2 marks & 0.5 for wrong
 
-        quiz.create("What is 1+1: ", "3", "2", "6", "4", 2);
-        quiz.create("In what year did India gain independence", "1947", "1950", "2012", "1948", 1);
+        quiz.create_mcq("What is 1+1: ", "3", "2", "6", "4", 2);
+        quiz.create_mcq("In what year did India gain independence", "1947", "1950", "2012", "1948", 1);
+        quiz.create_fib("______ + 5 = 8", "3");
 
         quiz.print_score();
+        quiz.grade(4);
     }
 }
