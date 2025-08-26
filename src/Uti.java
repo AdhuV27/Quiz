eb5dcc5f3958fa02e944c81386ae7fe48daa7532
Adhu <godadhu1212@gmail.com>
2025-08-26 14:30:40 +0530

    Commented

--- src/Uti.java
+++ src/Uti.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
 
 public class Uti {
-    Scanner in = new Scanner(System.in);
+    Scanner in = new Scanner(System.in); //initiate scanner
+
+    //initiate score and total
     public double score = 0;
     public double total = 0;
+
     public void create_mcq(String name, String op1, String op2, String op3, String op4, int CO) {
-        total += 1;
+        total += 1; //increase total by marks awarded for the question
+
+        //print
         System.out.println("Q: "+name + ": \n");
         System.out.println("Option 1: " + op1);
         System.out.println("Option 2: " + op2);
         System.out.println("Option 3: " + op3);
         System.out.println("Option 4: " + op4+"\n");
 
+        //accept
         System.out.println("Enter choice(number only): ");
         int choice = in.nextInt();
 
+        //checking
         if (choice == CO){
             System.out.println("Correct!");
             score+=1;
@@ -26,10 +33,14 @@ public class Uti {
     }
 
     public void create_fib(String q, String ans){
-        total += 2;
+        total += 2; //add marks awarded to score
+
+        //print and accept
         System.out.println("Q: "+q);
         System.out.println("Enter answer: ");
         String ua = in.next();
+        //ignore case is good for most questions
+        //planning to create new function which is case-sensitive
         if (ans.equalsIgnoreCase(ua)){
             System.out.println("Correct!!");
             score += 2;
@@ -40,18 +51,24 @@ public class Uti {
         }
     }
 
+    //just a failsafe for no particular reason
     public void setScore(double score) {
         this.score = score;
     }
 
+    //useful in future or in other functions like grade()
     public double getScore() {
         return score;
     }
 
+    //display score and grade
     public void print_score(){
         System.out.println(score+"\n");
         System.out.println(grade());
     }
+    //determine grade
+    // if full marks then A, if between total and total/2 ; B
+    //below
     public String grade(){
         double marks = getScore();
         return ((marks ==total)? "Grade: A" : (marks < total && marks >total/2.0) ? "Grade: B": "Grade: C" );
