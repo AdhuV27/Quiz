4e294f06b15f6a8aa8bf88885e9bb02f12f9cd3d
Adhu <godadhu1212@gmail.com>
2025-08-26 12:10:40 +0530

    Updated accordingly

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         quiz.create_mcq("What is 1+1: ", "3", "2", "6", "4", 2);
         quiz.create_mcq("In what year did India gain independence", "1947", "1950", "2012", "1948", 1);
         quiz.create_fib("______ + 5 = 8", "3");
+        quiz.create_fib("What is Einstein's first name: ", "albert");
 
         quiz.print_score();
-        quiz.grade(4);
     }
 }
